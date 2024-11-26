ebb2ea30e68da8344b8f4749887eb31057ca01c6
sihooh <khy2001smy@gmail.com>
2024-11-26 12:37:31 +0900

    v 1.1

--- src/main/java/com/lec/spring/HomeController.java
+++ src/main/java/com/lec/spring/HomeController.java
@@ -20,6 +20,6 @@ public class HomeController {
             log.warn("/aws/v1 호출. info 로그 ########################");
         }
 
-        return "<h1>AWS 배포 v1</h1>";
+        return "<h1>AWS 배포 v1.1</h1>";
     }
 }
